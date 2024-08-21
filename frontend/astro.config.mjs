c5a3d17a51436a05be62752f71768e127b855238
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-08-21 16:45:14 +0200

    Implemented backend and app base
    - [x] implemented github auth
    - [x] added some base components like toast, avatar from shadcn
    - [x] added tailwindcss
    - [x] added some layout-routes

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,37 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import { defineConfig, envField } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  experimental: {
+    actions: true,
+    env: {
+      schema: {
+        PUBLIC_APP_BASE_URL: envField.string({
+          context: "server",
+          access: "public",
+          default: "http://localhost:4321",
+        }),
+        PB_URL: envField.string({
+          context: "server",
+          access: "secret",
+          default: "localhost:8080",
+        }),
+      },
+    },
+  },
+});
---
+++ frontend/src/components/navigation/sidebar.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Logo } from "@/components/navigation/logo";
+import { SidebarRoutes } from "./sidebar-routes";
+
+interface SidebarProps {
+  loggedIn: boolean;
+}
+
+export const Sidebar = ({ loggedIn }: SidebarProps) => {
+  return (
+    <div className="flex h-full w-full flex-col overflow-y-auto border-r shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+      <div className="flex h-full w-full flex-col">
+        <SidebarRoutes loggedIn={loggedIn} />
+      </div>
+    </div>
+  );
+};
